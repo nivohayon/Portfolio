cf427998ff54f32318ce73236208bdb091f1e37e
nivohayon <nivohayon1582@gmail.com>
2021-12-10 18:03:52 +0200

    Added Mobile Support On Email Me Button

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,11 +6,16 @@ import "../Contact/Contact.css"
 function Contact() {
     return (
         <Container id="Contact">
-            <h1 id="title" onClick={()=>{
-                navigator.clipboard.writeText(contact.email)
-            }}>Contact</h1>
+            <h1 id="title">Contact</h1>
                 {contact && (
-                        <span type='button' title="nivohayon1582@gmail.com" className='contactbtn contactbtn--outline'>
+                        <span type='button'  onClick={()=>{
+                            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                                window.open(`mailto:${contact.email}`)
+                            }
+                            else {
+                                window.open(`https://mail.google.com/mail/?view=cm&source=mailto&to=${contact.email}`)
+                            }
+                        }} title="nivohayon1582@gmail.com" className='contactbtn contactbtn--outline'>
                         Email Me
                         </span>
                 )}
